b0185b09fe6e08122b4a954a05e8798bb5e6b0fc
Nicolás Carducci <nicolasc95@gmail.com>
2020-11-04 06:35:46 -0300

    name validation changed

--- JS/subscription.js
+++ JS/subscription.js
@@ -1,24 +1,33 @@
 window.onload = function(){
 
   var nameInput = document.querySelector('#name_input');
-  //Blur event = show error
-  nameInput.addEventListener("blur", nameFunction);
 
-  function nameFunction(){
-    var valueInput = nameInput.value;
-    if ((valueInput.indexOf(" ") == -1) || (valueInput.length <= 6)) { 
+  // Name ******* validation hidden at first
+  var errorMessage = document.querySelector('#name_input').nextElementSibling;
+    errorMessage.style.visibility =  "hidden";
+    
+  // Name ******* Blur event 
+  nameInput.addEventListener("blur", nameBlur);
+
+  function nameBlur(){
+    if ((!valName(this.value) || (this.value.length <= 6)) && this.value != '') { 
       var errorMessage = document.querySelector('#name_input').nextElementSibling;
       errorMessage.style.visibility =  "visible" ;
       errorMessage.style.color = "red";
     }else {
       console.log("Name: " + nameInput.value);
-      return ["Name: ",nameInput.value];
     }
   }
-  //Focus event = hidden error
-  nameInput.addEventListener("focus", hiddenName);
+  function valName(str){
+    let re = /^[a-zA-Z]+(?:\s[a-zA-Z]+)+$/;
+    if(re.test(str)){
+      return true;
+    }
+  }
+  // Name ******* Focus event
+  nameInput.addEventListener("focus", nameHidden);
 
-  function hiddenName(){
+  function nameHidden(){
     var errorMessage = document.querySelector('#name_input').nextElementSibling;
     errorMessage.style.visibility =  "hidden";
   }
