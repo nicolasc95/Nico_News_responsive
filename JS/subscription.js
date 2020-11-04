98052b5ca871d2004e66a812b2bee3e8e9e047e7
Nicolás Carducci <nicolasc95@gmail.com>
2020-11-04 06:50:35 -0300

    email validation

--- JS/subscription.js
+++ JS/subscription.js
@@ -1,11 +1,11 @@
 window.onload = function(){
 
+  /******************************** NAME ********************************/
   var nameInput = document.querySelector('#name_input');
-
   // Name ******* validation hidden at first
   var errorMessage = document.querySelector('#name_input').nextElementSibling;
     errorMessage.style.visibility =  "hidden";
-    
+
   // Name ******* Blur event 
   nameInput.addEventListener("blur", nameBlur);
 
@@ -29,6 +29,37 @@ window.onload = function(){
 
   function nameHidden(){
     var errorMessage = document.querySelector('#name_input').nextElementSibling;
+    errorMessage.style.visibility =  "hidden";
+  }
+
+/******************************** EMAIL ********************************/
+  var emailInput = document.querySelector('#email_input');
+  // Email ******* validation hidden at first
+  var errorMessage = document.querySelector('#email_input').nextElementSibling;
+    errorMessage.style.visibility =  "hidden";
+  // Email ******* Blur event 
+  emailInput.addEventListener("blur", emailBlur);
+
+  function emailBlur(){
+    if (!valEmail(this.value) && this.value != '') { 
+      var errorMessage = document.querySelector('#email_input').nextElementSibling;
+      errorMessage.style.visibility =  "visible" ;
+      errorMessage.style.color = "red";
+    }else {
+      console.log("Email: " + emailInput.value);
+    }
+  }
+  function valEmail(str){
+    let re =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(str)){
+      return true;
+    }
+  }
+  // Email ******* Focus event
+  emailInput.addEventListener("focus", emailHidden);
+
+  function emailHidden(){
+    var errorMessage = document.querySelector('#email_input').nextElementSibling;
     errorMessage.style.visibility =  "hidden";
   }
 }
